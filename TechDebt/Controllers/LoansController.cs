28970eb327ed501fed6ac100798ee43e6656da9d
Kayro25 <me0711405@gmail.com>
2022-11-30 15:37:17 -0300

    Cáclulo

---
+++ TechDebt/Controllers/LoansController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechDebt.Models;
+
+namespace TechDebt.Controllers
+{
+    public class LoansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Simulador()
+        {
+            var model = new LoanModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Simulador(LoanModel loan)
+        {
+            loan.Calculo();
+            return View(loan);
+        }
+
+
+    }
+}
---
+++ TechDebt/Models/LoanModel.cs
@@ -0,0 +1,23 @@
+﻿namespace TechDebt.Models
+{
+    public class LoanModel
+    {
+		public double Perfee { get; set; }
+		public double Valor { get; set; }
+		public int Anos { get; set; }
+		public double a { get; set; }
+		public double b { get; set; }
+		public double valorTotalPago { get; set; }
+		public double valorParcela { get; set; }
+		public double valorJuros { get; set; }
+
+		public void Calculo()
+		{
+			a = 1 + (Perfee / 10);
+			b = Math.Pow(a, Anos);
+			valorTotalPago = b * Valor;
+			valorParcela = valorTotalPago / (Anos * 12);
+			valorJuros = valorTotalPago - Valor;
+		}
+	}
+}
