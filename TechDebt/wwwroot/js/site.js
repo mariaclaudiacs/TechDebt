b9566b09689d333eacc4a576a128b4ec569daddc
João Vinícius Rosá <rosajv.blu@gmail.com>
2022-12-01 11:49:59 -0300

    input data de nascimento em consórcios

--- TechDebt/wwwroot/js/site.js
+++ TechDebt/wwwroot/js/site.js
@@ -2,4 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/* Bootstrap 5 JS included */
+/* vanillajs-datepicker 1.1.4 JS included */
 
+const getDatePickerTitle = elem => {
+
+    const label = elem.nextElementSibling;
+    let titleText = '';
+    if (label && label.tagName === 'LABEL') {
+        titleText = label.textContent;
+    } else {
+        titleText = elem.getAttribute('aria-label') || '';
+    }
+    return titleText;
+}
+
+const elems = document.querySelectorAll('.datepicker_input');
+for (const elem of elems) {
+    const datepicker = new Datepicker(elem, {
+        'format': 'dd/mm/yyyy', 
+        title: getDatePickerTitle(elem)
+    });
+}     
